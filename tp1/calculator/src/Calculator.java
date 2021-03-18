78b0ee7f144f52dd1df85af29cf6c58b3c595c76
Juan Pablo <juampigarcia2014@gmail.com>
2021-03-18 20:44:01 -0300

    Update Calculator.java

--- tp1/calculator/src/Calculator.java
+++ tp1/calculator/src/Calculator.java
@@ -8,38 +8,90 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        // Read an input from the user
         Scanner reader = new Scanner(System.in);
 
-        // List of values
-        ArrayList<Double> numbers = new ArrayList<>();
+        String operator = "";
 
-        // repeat the block until the block is exited
         while (true) {
-            // Ask the user for an input
-            System.out.println("Input a number, 0 to quit");
-            // Read the input of user
-            double number = Double.valueOf(reader.nextLine());
-            // Add value to list
-            numbers.add(number);
-            // Check if the loop should be exited
-            if (number == 0) {
+
+            System.out.println("Operator? (+ - * /)");
+
+            operator = reader.nextLine();
+
+            if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
                 break;
+            } else {
+                System.err.println("Invalid operator.");
             }
 
         }
 
-        double sum = sum(numbers);
-        double subtraction = subtraction(numbers);
-        double multiplication = multiplication(numbers);
+        boolean onlyTwoNumbers = false;
 
-        System.out.println("The result of the sum is: " + sum);
-        System.out.println("The result of the subtraction is: " + subtraction);
-        System.out.println("The result of the multiplication is: " + multiplication);
+        if (operator.equals("/")) {
+            onlyTwoNumbers = true;
+        }
+
+        ArrayList<Double> numbers = new ArrayList<>();
+
+        System.out.println("Input a number:");
+
+        double firstNumber = reader.nextDouble();
+
+        while (true) {
+
+            if ((onlyTwoNumbers) && (numbers.size() == 1)) {
+                if (numbers.get(0) == 0) {
+                    numbers.remove(0);
+                    System.err.println("The denominator can not be zero");
+                    continue;
+                }
+                break;
+            }
+
+            System.out.println("\nInput a number:");
+
+            if (!reader.hasNextDouble()) {
+                break;
+            }
+
+            double number = reader.nextDouble();
+
+            numbers.add(number);
+        }
+
+        reader.close();
+
+        switch (operator) {
+
+        case "+":
+            double sum = sum(firstNumber, numbers);
+            System.out.println("\nThe result of the sum is: " + sum);
+            break;
+
+        case "-":
+            double subtraction = subtraction(firstNumber, numbers);
+            System.out.println("The result of the subtraction is: " + subtraction);
+            break;
+
+        case "*":
+            double multiplication = multiplication(firstNumber, numbers);
+            System.out.println("The result of the multiplication is: " + multiplication);
+            break;
+
+        case "/":
+            double division = division(firstNumber, numbers);
+            System.out.println("The result of the division is: " + division);
+            break;
+
+        default:
+            break;
+        }
+
     }
 
-    private static double sum(ArrayList<Double> numbers) {
-        double ret = 0;
+    private static double sum(Double firstNumber, ArrayList<Double> numbers) {
+        double ret = firstNumber;
         if (!numbers.isEmpty()) {
             for (double number : numbers) {
                 ret += number;
@@ -48,8 +100,8 @@ public class Calculator {
         return ret;
     }
 
-    private static double subtraction(ArrayList<Double> numbers) {
-        double ret = 0;
+    private static double subtraction(Double firstNumber, ArrayList<Double> numbers) {
+        double ret = firstNumber;
         if (!numbers.isEmpty()) {
             for (Double number : numbers) {
                 ret -= number;
@@ -59,13 +111,18 @@ public class Calculator {
 
     }
 
-    private static double multiplication(ArrayList<Double> numbers) {
-        double ret=1;
+    private static double multiplication(Double firstNumber, ArrayList<Double> numbers) {
+        double ret = firstNumber;
         if (!numbers.isEmpty()) {
             for (Double number : numbers) {
                 ret = ret * number;
             }
         }
         return ret;
+    }
+
+    private static double division(Double numerator, ArrayList<Double> numbers) {
+        double denominator = numbers.get(0);
+        return numerator / denominator;
     }
 }
