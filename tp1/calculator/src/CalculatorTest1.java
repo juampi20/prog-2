cceed0d5e4f7331cda494915c48aef579887ebd9
Juan Pablo <juampigarcia2014@gmail.com>
2021-03-23 17:19:38 -0300

    Testing another calculator

--- tp1/calculator/src/CalculatorTest1.java
+++ tp1/calculator/src/CalculatorTest1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /**
  * Calculator
  */
-public class Calculator2 {
+public class CalculatorTest1 {
 
     public static void main(String[] args) {
 
--- tp1/calculator/src/CalculatorTest2.java
+++ tp1/calculator/src/CalculatorTest2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Calculator3 {
+public class CalculatorTest2 {
     public static void main(String[] args) {
         String operator;
         boolean run = true;
